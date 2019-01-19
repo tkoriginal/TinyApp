d8f994b3c22b9b7e4d9ec0ef6f9e5ef599580f4a
tkorignal <tauqueer.khan@gmail.com>
2019-01-19 22:15:41 +0000

    Fixed the copy issue that was only copying the first link

--- public/scripts/browser.js
+++ public/scripts/browser.js
@@ -1,9 +1,1 @@
 const copyBtn = new ClipboardJS('.btn');
-
-clipboard.on('click', function(e) {
-  console.info('Action:', e.action);
-  console.info('Text:', e.text);
-  console.info('Trigger:', e.trigger);
-
-  e.clearSelection();
-});
